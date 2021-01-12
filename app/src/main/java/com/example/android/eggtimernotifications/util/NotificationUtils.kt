480edd2ebc9d010b15ef299631375b95cc0e9d0b
Praveen <pkchmits@gmail.com>
2021-01-12 07:33:40 +0530

    Added pending intent for opening activity on notification click and dismiss the notification

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -17,8 +17,11 @@
 package com.example.android.eggtimernotifications.util
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.example.android.eggtimernotifications.MainActivity
 import com.example.android.eggtimernotifications.R
 
 // Notification ID.
@@ -35,10 +38,16 @@ private val FLAGS = 0
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
     // Create the content intent for the notification, which launches
     // this activity
-    // TODO: Step 1.11 create intent
+    // TODO DONE: Step 1.11 create intent
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    // TODO DONE: Step 1.12 create PendingIntent
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
-
-    // TODO: Step 1.12 create PendingIntent
 
     // TODO: Step 2.0 add style
 
@@ -53,16 +62,18 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
         .setSmallIcon(R.drawable.egg_icon)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
         .build()
 
-   // TODO DONE: Step 1.3 set title, text and icon to builder
+    // TODO DONE: Step 1.3 set title, text and icon to builder
 
 
     // TODO DONE: Step 1.8 use the new 'breakfast' notification channel
 
 
+    // TODO DONE: Step 1.13 set content intent
 
-    // TODO: Step 1.13 set content intent
 
     // TODO: Step 2.1 add style to builder
 
@@ -71,8 +82,8 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
     // TODO: Step 2.5 set priority
 
     // TODO DONE: Step 1.4 call notify
-    notify(NOTIFICATION_ID,notification)
+    notify(NOTIFICATION_ID, notification)
 
 }
 
-// TODO: Step 1.14 Cancel all notifications
+// TODO DONE: Step 1.14 Auto Cancel all notifications
