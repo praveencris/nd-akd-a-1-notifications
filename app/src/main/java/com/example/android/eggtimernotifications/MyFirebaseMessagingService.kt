691bce5eb8e62d2048c32527e7c278ccff570043
Praveen <pkchmits@gmail.com>
2021-01-12 17:09:13 +0530

    Added notification from FCM It only apper when app is in background either in onStoped state or not even in menu stack

---
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,36 @@
+package com.example.android.eggtimernotifications
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    companion object {
+        private val TAG = MyFirebaseMessagingService::class.java.simpleName
+    }
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+    }
+
+    // DONE: Step 3.2 log registration token
+    // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+
+    }
+}
